45ffb591d345f91ccce9d143f2e7c39831be4588
Kanra153 <mad_hatter153@bk.ru>
2017-07-23 21:58:36 +0300

    Добавил обработку текста и слов. Исправил названия переменных

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,18 +4,22 @@ import sys
 
 def load_data(filepath):
     with open(filepath, "r") as textfile:
-        text = textfile.read()
+        text = textfile.read().lower()
     return text
    
 def get_most_frequent_words(text):
-    words = text.split()
-    count = collections.Counter(words).most_common(10)
-    return count
+    words = re.findall(r'\w+', text)
+    count_words = collections.Counter(words).most_common(10)
+    return count_words
     
 if __name__ == '__main__':
     filepath = sys.argv[1]
     text = load_data(filepath)
-    count = get_most_frequent_words(text)
-    print('Список самых часто встречающихся слов:', count)
+    count_words = get_most_frequent_words(text)
+    print('Список самых часто встречающихся слов:')
+    for prettify in count_words:
+        print(prettify)
+    
+  
 
   
