e34ba86dc2489aa33e532b770c25d39452d2156b
Kanra153 <mad_hatter153@bk.ru>
2017-07-24 09:26:53 +0300

    Удалил глобальную переменную

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,24 +2,25 @@ import collections
 import re
 import sys
 
-words_num = 10
-
 def load_data(filepath):
     with open(filepath, "r") as textfile:
         text = textfile.read().lower()
     return text
    
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, words_num=10):
     words = re.findall(r'\w+', text)
     count_words = collections.Counter(words).most_common(words_num)
     return count_words
     
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    text = load_data(filepath)
-    count_words = get_most_frequent_words(text)
-    print('Список самых часто встречающихся слов:')
-    for prettify in count_words:
-        print(prettify)
+    try:
+        filepath = sys.argv[1]
+        text = load_data(filepath)
+        count_words = get_most_frequent_words(text)
+        print('Список самых часто встречающихся слов:')
+        for prettify in count_words:
+            print(prettify)
+    except IndexError:
+        print('Вы не ввели путь до текстового файла')
     
   
